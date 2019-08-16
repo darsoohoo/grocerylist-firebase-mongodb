0a501c1ae240241a764ef10df9b870ada291092e
Darren SooHoo <darsoohoo@gmail.com>
2019-08-16 14:46:00 -0700

    add title to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ export default class Footer extends Component {
             <div>
                 <footer class="mdl-mini-footer">
                 <div class="mdl-mini-footer__left-section">
-                    <div class="mdl-logo">Title</div>
+                    <div class="mdl-logo">Shopping List</div>
                     <ul class="mdl-mini-footer__link-list">
                     <li><a href="#">Help</a></li>
                     <li><a href="#">Privacy & Terms</a></li>
