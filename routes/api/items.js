ded5d40da4b8545031d5cb0b4030a4514e330ec1
Darren SooHoo <darsoohoo@gmail.com>
2019-08-19 20:18:40 -0700

    make prettier on all files

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,34 +1,29 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
-const Item = require('../../models/Item')
-
+const Item = require('../../models/Item');
 
 router.get('/', async (req, res) => {
-    try {
-      const items = await Item.find().sort({ date: -1 });
-      res.json(items);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
-    }
+  try {
+    const items = await Item.find().sort({ date: -1 });
+    res.json(items);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
-
 
 router.post('/', (req, res) => {
-    const newItem = new Item({
-        item: req.body.itemInput,
-        purchased: req.body.purchased
-    })
-
-    newItem.save().then(item => res.json(item));
+  const newItem = new Item({
+    item: req.body.itemInput,
+    purchased: req.body.purchased
+  });
+  newItem.save().then(item => res.json(item));
 });
 
-
-
 router.delete('/:id', (req, res) => {
-      Item.findByIdAndDelete(req.params.id)
-      .then(() => res.json('Item deleted.'))
-      .catch(err => res.status(400).json('Error: ' + err));
+  Item.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Item deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/update/:id').post((req, res) => {
@@ -36,14 +31,12 @@ router.route('/update/:id').post((req, res) => {
     .then(item => {
       item.item = req.body.newItem;
       item.purchased = req.body.purchased;
-      item.save()
+      item
+        .save()
         .then(() => res.json('Item updated!'))
         .catch(err => res.status(400).json('Error: ' + err));
     })
     .catch(err => res.status(400).json('Error: ' + err));
 });
-
-
-
 
 module.exports = router;
